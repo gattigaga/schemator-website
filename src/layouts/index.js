6f9c7f3a5552d837b0acec9b8544066c0115bb72
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-08-29 22:19:42 +0700

    Remove unneeded resources

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,31 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
-import Header from '../components/header'
-import './index.css'
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`
 
-const Layout = ({ children, data }) => (
-  <div>
+const Layout = ({ children }) => (
+  <Container>
     <Helmet
-      title={data.site.siteMetadata.title}
+      title="Schemator 2"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+    {children()}
+  </Container>
 )
 
 Layout.propTypes = {
@@ -33,13 +26,3 @@ Layout.propTypes = {
 }
 
 export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
